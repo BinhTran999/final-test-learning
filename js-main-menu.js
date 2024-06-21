11e70582b081a391c9adc541dd67e548720df75c
63200296 - Tran Thanh Binh <tranthanhbinh171002@lgcns.com>
2024-06-21 04:02:57 -0700

    terdsytfuyg

--- js-main-menu.js
+++ js-main-menu.js
@@ -6,59 +6,73 @@ const facilities_type_click = document.getElementById(
 const itemArr = [...document.querySelectorAll(".item .img-html")];
 const itemNameArr = [...document.querySelectorAll(".item .text-content")];
 const itemPriceArr = [...document.querySelectorAll(".item .price")];
-const itemArrEmpty = [...document.querySelectorAll(".item .img-html")];
-const itemNameArrEmpty = [...document.querySelectorAll(".item .text-content")];
-const itemPriceArrEmpty = [...document.querySelectorAll(".item .price")];
 const shoppingItemList = document.getElementById("myBox");
 
-console.log(itemNameArr);
-
-function get_data(url) {
-  return fetch(url)
-    .then((response) => response.json())
-    .then((webData) => log(webData))
-    .catch((error) => {
-      console.error("Lỗi:", error);
-    });
+async function funcRequest(url, maxRetries = 10, retryDelay = 5) {
+  let retries = 0;
+  while (true) {
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        if (response.status === 500 && retries < maxRetries) {
+          await new Promise((resolve) => setTimeout(resolve, retryDelay));
+          retries++;
+          continue;
+        }
+        throw new Error(`HTTP error ${response.status}`);
+      }
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error("Error:", error);
+      if (error.message === "HTTP error 500" && retries < maxRetries) {
+        await new Promise((resolve) => setTimeout(resolve, retryDelay));
+        retries++;
+        continue;
+      }
+      throw error;
+    }
+  }
 }
 
 async function getEnermyData() {
-  return await get_data("http://10.63.161.172:3001/api/get-product");
+  allData = [];
+  const data = await funcRequest("http://10.63.161.172:3001/api/get-product");
+
+  for (
+    let i = 0;
+    i < data.data.total / Object.keys(data.data.items).length + 1;
+    i++
+  ) {
+    const url_i = "http://10.63.161.172:3001/api/get-product?page=" + i;
+    data_i = await funcRequest(url_i);
+    allData.push(...data_i.data.items);
+  }
+  return allData;
 }
 
-async function fetchData() {
-  let newData = ``;
-  try {
-    let data = await getEnermyData();
-    while (data.code != 200) {
-      data = await getEnermyData();
-    }
-    newData = data;
-    console.log(newData);
-    console.log(Object.keys(newData.data.items).length);
-  } catch (error) {
-    console.error("Lỗi:", error);
+async function processData() {
+  var cachedData = null;
+  if (cachedData !== null) {
+    return cachedData;
   }
-  let services = {};
-  let facilities = {};
-  let newArray_services = new Array();
-  let newArray_facilities = new Array();
-  services.data = newArray_services;
-  facilities.data = newArray_facilities;
-  for (let i = 0; i < Object.keys(newData.data.items).length; i++) {
-    if (newData.data.items[i].type === "service") {
-      services.data.push(newData.data.items[i]);
-    } else if (newData.data.items[i].type === "facility") {
-      facilities.data.push(newData.data.items[i]);
-    }
-    console.log(newData.data.items[i].name);
-  }
-  return [newData, services, facilities];
+  const data = await getEnermyData();
+  cachedData = data;
+
+  return cachedData;
 }
 
 async function serviceClick() {
-  console.log("111");
-  let [newData, services, facilities] = await fetchData();
+  let services = {};
+  let newArray_services = new Array();
+  services.data = newArray_services;
+  let data = await processData();
+  for (let i = 0; i < Object.keys(data).length; i++) {
+    if (data[i].type === "service") {
+      services.data.push(data[i]);
+    }
+  }
+  console.log(services.data);
   for (let i = 0; i < Object.keys(services.data).length; i++) {
     let bimg = "url('" + services.data[i].image + "')";
     itemArr[i].style.backgroundImage = bimg;
@@ -69,24 +83,31 @@ async function serviceClick() {
 }
 
 async function faciltitesClick() {
-  console.log("111");
-  let [newData, services, facilities] = await fetchData();
-  for (let i = 0; i < Object.keys(facilities.data).length; i++) {
-    let bimg = "url('" + facilities.data[i].image + "')";
+  let faciltites = {};
+  let newArray_faciltites = new Array();
+  faciltites.data = newArray_faciltites;
+  let data = await processData();
+  for (let i = 0; i < Object.keys(data).length; i++) {
+    if (data[i].type === "facility") {
+      faciltites.data.push(data[i]);
+    }
+  }
+  for (let i = 0; i < Object.keys(faciltites.data).length; i++) {
+    let bimg = "url('" + faciltites.data[i].image + "')";
     itemArr[i].style.backgroundImage = bimg;
     itemArr[i].style.backgroundSize = "100% 100%";
-    itemNameArr[i].innerHTML = facilities.data[i].name;
-    itemPriceArr[i].innerHTML = facilities.data[i].price + " VND";
+    itemNameArr[i].innerHTML = faciltites.data[i].name;
+    itemPriceArr[i].innerHTML = faciltites.data[i].price + " VND";
   }
 }
 
-function listItem() {
+async function listItem() {
+  console.log(Object.keys(shoppingItemList.style.display).length);
+  console.log(shoppingItemList.style.display);
   document.getElementById("shopping").addEventListener("click", function () {
-    console.log(shoppingItemList.style.display);
-    // document.getElementById("myBox").style.display = "none";
     if (shoppingItemList.style.display === "none") {
       shoppingItemList.style.display = "block";
-    } else {
+    } else if (shoppingItemList.style.display === "block") {
       shoppingItemList.style.display = "none";
     }
   });
