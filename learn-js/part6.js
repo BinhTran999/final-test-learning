4c0a67bc3d73ddaa386e13ccee33b0e1e99581e0
63200296 - Tran Thanh Binh <tranthanhbinh171002@lgcns.com>
2024-07-10 05:32:55 -0700

    asasas

---
+++ learn-js/part6.js
@@ -0,0 +1,19 @@
+const person = { name: 'John', age: 30, city: 'New York' };
+
+console.log(Object.keys(person)); // Output: ['name', 'age', 'city']
+console.log(Object.values(person)); // Output: ['John', 30, 'New York']
+console.log(Object.entries(person));
+console.log(typeof Object.values(person));
+// Output: [['name', 'John'], ['age', 30], ['city', 'New York']]
+
+console.log(Object.entries({}))
+console.log(Object.keys([2,3,4,5]))
+
+console.log(typeof Object.keys([2,3,4,5]))
+console.log(Object.values([2,3,4,5]))
+console.log(Object.entries([2,3,4,5]))
+
+console.log(Object.entries([2,3,4,5]))
+
+console.log(Object.keys(2))
+
